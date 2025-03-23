eb474800b80cc2765c3d0d9d168e29f6451d4cde
sinnilay <singhnilay55@gmail.com>
2025-03-23 11:34:32 +0530

    bug

--- src/GEMINI-API.js
+++ src/GEMINI-API.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY="AIzaSyApECE9JixZkl0xLwB-a9JwpiKRmXk_e8c"
+const API_KEY="AIzaSyBL3vxxaBumcVAXbS_wq03OuBZ_yybFFsA"
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
